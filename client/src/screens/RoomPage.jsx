a5f45749f472c436496e2644a54b3d0132bee7e6
Abhishekkv <abhishekkvpnld@gmail.com>
2024-05-13 14:45:48 +0530

    connection created

--- client/src/screens/RoomPage.jsx
+++ client/src/screens/RoomPage.jsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useSocket } from '../context/SocketProvider';
+import Peer from '../service/peer';
 import ReactPlayer from "react-player";
+import peer from '../service/peer';
 
 
 const RoomPage = () => {
@@ -15,22 +17,50 @@ const RoomPage = () => {
     setRemoteSocketId(id);
   }, []);
 
+
   const handleCallUser = useCallback(async () => {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
       video: true
     });
+    const offer = await Peer.getOffer();
+    socket.emit("user:call", { to: remoteSocketId, offer });
     setMyStream(stream);
+  }, [remoteSocketId, socket]);
+
+
+  const handleIncomingCall = useCallback(async ({ from, offer }) => {
+    setRemoteSocketId(from);
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true
+    });
+    setMyStream(stream);
+    console.log(from, offer);
+    const ans = await peer.getAnswer(offer);
+    socket.emit("call:accepted", { to: from, ans });
+  }, [socket]);
+
+
+  const handleCallAccepted = useCallback( ({ from, ans }) => {
+    peer.setLocalDescription(ans);
+    console.log("Call Accepted");
   }, []);
 
   useEffect(() => {
     socket.on("user:joined", handleUserJoined);
+    socket.on("incoming:call", handleIncomingCall);
+    socket.on("call:accepted", handleCallAccepted);
 
     return () => {
       socket.off("user:joined", handleUserJoined);
+      socket.off("incoming:call", handleIncomingCall);
+      socket.off("call:accepted", handleCallAccepted);
     };
 
   }, [socket, handleUserJoined]);
+
+
 
   return (
     <div>
--- server/index.js
+++ server/index.js
@@ -32,17 +32,28 @@ const socketIdToEmailMap = new Map();
 io.on("connection", (socket) => {
     console.log('socket connected', socket.id);
 
+
     socket.on("room:join", (data) => {
         const { email, room } = data;
         emailToSocketIdMap.set(email, socket.id);
         socketIdToEmailMap.set(socket.id, email);
-        io.to(room).emit("user:joined",{email,id:socket.id});
+        io.to(room).emit("user:joined", { email, id: socket.id });
         socket.join(room);
         io.to(socket.id).emit("room:join", data);
-    })
+    });
 
-}); 
 
-// httpServer.listen(5000,()=>{ 
+    socket.on("user:call", ({ to, offer }) => {
+        io.to(to).emit("incoming:call", { from: socket.id, offer });
+    });
+
+
+    socket.on("call:accepted", ({ to,ans}) => {
+        io.to(to).emit("call:accepted", { from: socket.id, ans });
+    });
+
+});
+
+// httpServer.listen(5000,()=>{
 // console.log("Server connected");
 // });
